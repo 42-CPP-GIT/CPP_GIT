410b012c62cd6263ae3ab3b53cb901267eb2e6fc
aLVINlEE9 <fldh3746@gmail.com>
2022-10-25 12:38:33 +0900

    ex00

---
+++ seungsle/CPP00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungsle <seungsle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 12:23:11 by seungsle          #+#    #+#             */
+/*   Updated: 2022/10/25 12:36:53 by seungsle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" ;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				std::cout << (char)toupper(argv[i][j]);
+			}
+		}
+		std::cout << "\n";
+	}
+	return (0);
+}
