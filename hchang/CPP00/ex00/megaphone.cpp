1eec8eab31d89467eff8e39db66f55583e6842d3
change-challenge <dog941128@hotmail.com>
2022-10-27 18:35:59 +0900

    [hchang]CPP00 OFFLINE REVIEW DONE

--- hchang/CPP00/ex00/megaphone.cpp
+++ hchang/CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:18:04 by hchang            #+#    #+#             */
-/*   Updated: 2022/10/21 16:44:00 by hchang           ###   ########.fr       */
+/*   Updated: 2022/10/27 17:34:14 by hchang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@
 int main(int argc, char** argv)
 {
     std::string str;
-	auto a;
 	
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < argc; i++)
 			str += std::string(argv[i]);
 		for (int j = 0; j < str.size(); j++)
 			str[j] = std::toupper(str[j]);
-		std::cout << str << std::endl;
+		std::cout << str;
 	}
+	std::cout << std::endl;
 	return (0);
 }
