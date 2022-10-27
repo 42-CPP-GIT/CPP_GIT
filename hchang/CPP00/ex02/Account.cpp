c29eb7ba1f38e8c59cd81def01678bac3b9ead96
change-challenge <dog941128@hotmail.com>
2022-10-27 12:47:35 +0900

    ADD ex02

--- hchang/CPP00/ex02/Account.cpp
+++ hchang/CPP00/ex02/Account.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hojinjang <hojinjang@student.42.fr>        +#+  +:+       +#+        */
+/*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 15:18:04 by hchang            #+#    #+#             */
-/*   Updated: 2022/10/26 23:42:53 by hojinjang        ###   ########.fr       */
+/*   Updated: 2022/10/27 12:47:22 by hchang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ Account::Account(int initial_deposit)
 {
 	this->_accountIndex = _nbAccounts;	
 	this->_amount = initial_deposit;
+	_totalAmount += this->_amount;
 	
 	Account::_displayTimestamp();
 	std::cout << " index:" << this->_accountIndex;
@@ -33,13 +34,68 @@ Account::Account(int initial_deposit)
 
 Account::~Account( void )
 {
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount << ";closed" << std::endl;
 
 }
 
 void	Account::displayAccountsInfos()
 {
-	
+	Account::_displayTimestamp();
+
+	std::cout << " accounts:" << _nbAccounts << ";total:" << _totalAmount
+	<< ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
+
+void	Account::makeDeposit( int deposit )
+{
+	Account::_displayTimestamp();
+
+	this->_nbDeposits++;
+	_totalNbDeposits++;
+	this->_amount += deposit;
+	_totalAmount += deposit;
+	std::cout << " index:" << this->_accountIndex << ";p_amount:" 
+	<< this->_amount - deposit << ";deposit:" << deposit << ";amount:" << this->_amount
+	<< ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	Account::_displayTimestamp();
+
+	if (withdrawal > this->_amount)
+	{
+		std::cout << " index:" << this->_accountIndex << ";p_amount:" 
+		<< this->_amount << ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+	else
+	{
+		this->_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		this->_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		std::cout << " index:" << this->_accountIndex << ";p_amount:" 
+		<< this->_amount + withdrawal << ";withdrawal:" << withdrawal << ";amount:" << this->_amount
+		<< ";nb_withdrawals:" << this->_nbDeposits << std::endl;
+		return (true);
+	}
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
+
+void	Account::displayStatus( void ) const
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount
+	<< ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
 
 int	Account::getNbAccounts( void )
 {
--- hchang/CPP00/ex02/tests.cpp
+++ hchang/CPP00/ex02/tests.cpp
@@ -38,27 +38,27 @@ int		main( void ) {
 	ints_t::iterator	wit_end		= withdrawals.end();
 
 	Account::displayAccountsInfos();
-	// std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
-	// for ( acc_int_t it( acc_begin, dep_begin );
-    //         it.first != acc_end && it.second != dep_end;
-    //         ++(it.first), ++(it.second) ) {
+	for ( acc_int_t it( acc_begin, dep_begin );
+            it.first != acc_end && it.second != dep_end;
+            ++(it.first), ++(it.second) ) {
 
-	// 	(*(it.first)).makeDeposit( *(it.second) );
-	// }
+		(*(it.first)).makeDeposit( *(it.second) );
+	}
 
-	// Account::displayAccountsInfos();
-	// std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
-	// for ( acc_int_t it( acc_begin, wit_begin );
-    //         it.first != acc_end && it.second != wit_end;
-    //         ++(it.first), ++(it.second) ) {
+	for ( acc_int_t it( acc_begin, wit_begin );
+            it.first != acc_end && it.second != wit_end;
+            ++(it.first), ++(it.second) ) {
 
-	// 	(*(it.first)).makeWithdrawal( *(it.second) );
-	// }
+		(*(it.first)).makeWithdrawal( *(it.second) );
+	}
 
-	// Account::displayAccountsInfos();
-	// std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	return 0;
 }
