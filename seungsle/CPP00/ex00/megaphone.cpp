cbd800fe9669ccfb258862dfda87faa917e5687a
aLVINlEE9 <fldh3746@gmail.com>
2022-11-01 14:41:44 +0900

    fix not using stl

--- seungsle/CPP00/ex00/megaphone.cpp
+++ seungsle/CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 12:23:11 by seungsle          #+#    #+#             */
-/*   Updated: 2022/10/29 21:00:26 by seungsle         ###   ########.fr       */
+/*   Updated: 2022/11/01 14:40:42 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,19 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
+		std::string str;
 		for (int i = 1; i < argc; i++)
-		{
-			std::string argvString(argv[i]);
-			for (std::string::iterator it = argvString.begin(); it != argvString.end(); ++it)
-				*it = std::toupper(*it);
-			std::cout << argvString;
-		}
+			str += std::string(argv[i]);
+		for (size_t i = 0; i < str.length(); i++)
+			str[i] = std::toupper(str[i]);
+		std::cout << str;
+		// for (int i = 1; i < argc; i++)
+		// {
+		// 	std::string argvString(argv[i]);
+		// 	for (std::string::iterator it = argvString.begin(); it != argvString.end(); ++it)
+		// 		*it = std::toupper(*it);
+		// 	std::cout << argvString;
+		// }
 		std::cout << std::endl;
 	}
 	return (0);
