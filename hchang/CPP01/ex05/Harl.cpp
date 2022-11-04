c90a365e763fe997bb46a439fea133f9c82f8225
change-challenge <dog941128@hotmail.com>
2022-11-04 18:22:51 +0900

    [Feat]:ex05

---
+++ hchang/CPP01/ex05/Harl.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 17:00:45 by hchang            #+#    #+#             */
+/*   Updated: 2022/11/04 18:22:30 by hchang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::debug(void)
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger ! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	f[0] = &Harl::debug;
+	f[1] = &Harl::info;
+	f[2] = &Harl::warning;
+	f[3] = &Harl::error;
+
+	std::string str = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	str = "[DEBUG]INFO"
+	switch (expression)
+	{
+	case /* constant-expression */:
+		/* code */
+		break;
+	
+	default:
+		break;
+	}
+}
---
+++ hchang/CPP01/ex05/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 17:00:33 by hchang            #+#    #+#             */
+/*   Updated: 2022/11/04 17:58:21 by hchang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP_
+#define HARL_HPP_
+
+#include <iostream>
+
+class Harl
+{
+  private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+
+  public:
+  	void (Harl::*f[4])(void);
+    void complain(std::string level);
+    Harl();
+    ~Harl();
+};
+
+#endif
