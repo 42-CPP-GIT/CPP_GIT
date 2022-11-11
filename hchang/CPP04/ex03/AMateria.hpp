6628b7be26b37ffdd869e6bf5ed2a2d24d92289e
change-challenge <dog941128@hotmail.com>
2022-11-11 16:42:44 +0900

    [Init] ex03

---
+++ hchang/CPP04/ex03/AMateria.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 16:31:45 by hchang            #+#    #+#             */
+/*   Updated: 2022/11/11 16:35:37 by hchang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+#include <iostream>
+
+class ICharacter;
+
+class AMateria
+{
+	protected:
+		std::string _type;
+
+	public:
+		AMateria(std::string const & type);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
---
+++ hchang/CPP04/ex03/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 16:34:11 by hchang            #+#    #+#             */
+/*   Updated: 2022/11/11 16:37:01 by hchang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "IMateriaSource.hpp"
+#include "ICharacter.hpp"
+
+int main()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	
+	delete bob;
+	delete me;
+	delete src;
+	
+	return 0;
+}
