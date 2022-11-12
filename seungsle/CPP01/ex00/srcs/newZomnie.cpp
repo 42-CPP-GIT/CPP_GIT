64cfa42bcdd3f5d38917509a89f44e6589c7db77
aLVINlEE9 <fldh3746@gmail.com>
2022-11-12 18:33:23 +0900

    fixed CPP01

--- seungsle/CPP01/ex00/srcs/newZomnie.cpp
+++ seungsle/CPP01/ex00/srcs/newZomnie.cpp
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   newZomnie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/26 11:49:54 by seungsle          #+#    #+#             */
-/*   Updated: 2022/11/12 18:29:50 by seungsle         ###   ########.fr       */
+/*   Created: 2022/11/12 18:26:38 by seungsle          #+#    #+#             */
+/*   Updated: 2022/11/12 18:27:00 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	Zombie::announce()
+Zombie*	Zombie::newZombie(std::string name)
 {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-Zombie::Zombie(std::string name)
-{
-	_name = name;
-}
-
-Zombie::~Zombie()
-{
-	std::cout << _name << " is destructed" << std::endl;
-}
-
-Zombie::Zombie()
-{
-	_name = "Foo";
+	Zombie *newZombie = new Zombie(name);
+	return (newZombie);
 }
--- seungsle/CPP01/ex00/srcs/randomChump.cpp
+++ seungsle/CPP01/ex00/srcs/randomChump.cpp
@@ -1,33 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/26 11:49:54 by seungsle          #+#    #+#             */
-/*   Updated: 2022/11/12 18:29:50 by seungsle         ###   ########.fr       */
+/*   Created: 2022/11/12 18:27:16 by seungsle          #+#    #+#             */
+/*   Updated: 2022/11/12 18:27:25 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	Zombie::announce()
+void	Zombie::randomChump(std::string name)
 {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
+	Zombie	*zombie;
 
-Zombie::Zombie(std::string name)
-{
-	_name = name;
-}
-
-Zombie::~Zombie()
-{
-	std::cout << _name << " is destructed" << std::endl;
-}
-
-Zombie::Zombie()
-{
-	_name = "Foo";
+	zombie = newZombie(name);
+	zombie->announce();
+	delete zombie;
 }
