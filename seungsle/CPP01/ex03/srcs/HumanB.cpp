f15fa62c19c95b6f204cae84f1c7b0e2fdeb140d
aLVINlEE9 <fldh3746@gmail.com>
2022-11-14 18:55:29 +0900

    fix not setted case

--- seungsle/CPP01/ex03/srcs/HumanB.cpp
+++ seungsle/CPP01/ex03/srcs/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 21:14:58 by seungsle          #+#    #+#             */
-/*   Updated: 2022/11/01 16:17:11 by seungsle         ###   ########.fr       */
+/*   Updated: 2022/11/14 18:54:38 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 HumanB::HumanB(std::string name)
 {
 	this->_name = name;
+	this->_weapon = NULL;
 }
 
 HumanB::HumanB()
@@ -34,5 +35,8 @@ void	HumanB::setWeapon(Weapon &weapon)
 
 void	HumanB::attack()
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	if (this->_weapon == NULL)
+		std::cout << "weapon not setted" << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 }
